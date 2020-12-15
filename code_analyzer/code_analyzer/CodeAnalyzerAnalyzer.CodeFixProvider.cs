0e4528776a1769695cb5cbe1888f52184de2cf07
Muhammad Daniyal Khan <mdaniyalkhann@gmail.com>
2020-12-15 16:26:02 +0500

    Update CodeAnalyzerAnalyzer.CodeFixProvider.cs

--- code_analyzer/code_analyzer/CodeAnalyzerAnalyzer.CodeFixProvider.cs
+++ code_analyzer/code_analyzer/CodeAnalyzerAnalyzer.CodeFixProvider.cs
@@ -21,8 +21,6 @@ namespace code_analyzer
     {
         private const string PrivateModifier = "private";
         private const string Title = "Encapsulate Field";
-        
-        
         private const string TitleMagicValues = "Replace Magic Values";
         private const string FieldPrefix = "_";
         private const string String = "string";
